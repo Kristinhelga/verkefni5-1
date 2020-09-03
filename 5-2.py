ea802804bbddfee17921c711affcc0366d4c89bb
Kristín Thorsteinsdóttir <kristinthorsteinsdottir@Kristins-MacBook-Pro-5.local>
2020-09-03 11:14:39 +0000

    Adding coding

--- 5-2.py
+++ 5-2.py
@@ -1,9 +1,20 @@
 
 # Get input from user
+# Iterate through the sequence 
+# Print the length of the sequence from user input
 
 
 
 n = int(input("Enter the length of the sequence: ")) 
 
-sequence = (1,2,3,6,11,20,37)
-print (sequence)
+first = 1
+second = 2
+third = 3
+print(first, second, third, end=' ')
+
+for i in range(4,n+1):
+    current = first + second + third
+    print(current, end= ' ')
+    first = second
+    second = third
+    third = current
