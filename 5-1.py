f3f07442014ea5875ee8960deafa5ae4a5fc811f
Kristín Thorsteinsdóttir <kristinthorsteinsdottir@Kristins-MacBook-Pro-5.local>
2020-09-03 10:15:33 +0000

    Adding coding

---
+++ 5-1.py
@@ -0,0 +1,23 @@
+
+# Get input from user
+# Find the max int from user until a negative value is entered
+# print the max value
+
+ 
+
+max_int = 0
+
+while True:
+    num_int = int(input("Input a number: "))
+
+    if num_int >= max_int:
+        max_int = num_int
+
+    if num_int <= 0:
+        break
+
+print('The maximus is', max_int)
+
+
+
+
